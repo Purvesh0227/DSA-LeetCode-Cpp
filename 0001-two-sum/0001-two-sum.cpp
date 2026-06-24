e1ae70a62dfe4ad60eb69b6dcc929d28f84848dc
Purvesh Mohite <purvesh0207@gmail.com>
2026-06-24 15:57:29 +0530

    Time: 4 ms (51.8%), Space: 15 MB (12.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,49 +1,65 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         // Pairing  each element with its index
+//         // vector<pair<int,int>> arr;
+//         // for(int i = 0; i < nums.size(); i++) {
+//         //     arr.push_back({nums[i], i});
+//         // }
+
+       
+//         // sort(arr.begin(), arr.end());
+
+        
+//         // int start = 0;
+//         // int end = arr.size() - 1;
+
+//         // while(start < end) {
+//         //     int sum = arr[start].first + arr[end].first;
+//         //     if(sum == target) {
+//         //         return {arr[start].second, arr[end].second}; // 
+//         //     }
+//         //     else if(sum > target) {
+//         //         end--;
+//         //     }
+//         //     else {
+//         //         start++;
+//         //     }
+//         // }
+//         // return {};
+
+//         unordered_map<int,int>m;
+//         vector<int> ans;
+//         int n = nums.size();
+
+//         for(int i = 0;i<n;i++)
+//         {
+//             int first = nums[i];
+//             int second = target - first;
+//             if(m.find(second)!=m.end())
+//             {
+//                 ans.push_back(i);
+//                 ans.push_back(m[second]);
+//                 break;
+//             }
+//             m[first] = i;
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Pairing  each element with its index
-        // vector<pair<int,int>> arr;
-        // for(int i = 0; i < nums.size(); i++) {
-        //     arr.push_back({nums[i], i});
-        // }
-
-       
-        // sort(arr.begin(), arr.end());
-
-        
-        // int start = 0;
-        // int end = arr.size() - 1;
-
-        // while(start < end) {
-        //     int sum = arr[start].first + arr[end].first;
-        //     if(sum == target) {
-        //         return {arr[start].second, arr[end].second}; // 
-        //     }
-        //     else if(sum > target) {
-        //         end--;
-        //     }
-        //     else {
-        //         start++;
-        //     }
-        // }
-        // return {};
-
-        unordered_map<int,int>m;
-        vector<int> ans;
+        map<int,int> mpp;
         int n = nums.size();
-
-        for(int i = 0;i<n;i++)
-        {
-            int first = nums[i];
-            int second = target - first;
-            if(m.find(second)!=m.end())
-            {
-                ans.push_back(i);
-                ans.push_back(m[second]);
-                break;
+        for(int i=0;i<n;i++){
+            int num = nums[i];
+            int more = target-num;
+            if(mpp.find(more)!=mpp.end()){
+                return {mpp[more],i};
             }
-            m[first] = i;
+            mpp[num]=i;
         }
-        return ans;
+        return {-1,-1};
     }
 };
