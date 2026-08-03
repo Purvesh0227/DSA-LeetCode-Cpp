a721c72c62495331bd72adbecfc06861dac30dfc
Purvesh Mohite <purvesh0207@gmail.com>
2026-08-03 16:05:03 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> arr;  
-        int count = 0;    
+        // int n = nums.size();
+        // vector<int> arr;  
+        // int count = 0;    
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) {
-                arr.push_back(nums[i]); 
-            } else {
-                count++; 
-            }
-        }
-
- 
-        for (int i = 0; i < count; i++) {
-            arr.push_back(0); 
-        }
-
-        nums = arr;  
-        // int j = 0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i]!=0){
-        //         swap(nums[i],nums[j]);
-        //         j++;   
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] != 0) {
+        //         arr.push_back(nums[i]); 
+        //     } else {
+        //         count++; 
         //     }
         // }
+
+ 
+        // for (int i = 0; i < count; i++) {
+        //     arr.push_back(0); 
+        // }
+
+        // nums = arr;  
+        int i = 0;
+        for(int j=0;j<nums.size();j++){
+            if(nums[j]!=0){
+                swap(nums[i],nums[j]);
+                i++;   
+            }
+        }
     }
 };
