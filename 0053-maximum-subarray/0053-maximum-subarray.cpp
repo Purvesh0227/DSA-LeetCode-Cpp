131631932362378d8b17dc8dff241d3a2758d5a5
Purvesh Mohite <purvesh0207@gmail.com>
2026-08-03 15:18:00 +0530

    Time: 0 ms (100%), Space: 71.6 MB (98.84%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -16,13 +16,30 @@ public:
     //     return maxsum;
         
     // }
-    int n = nums.size();
-    int maxi = nums[0];
-    int curr  = nums[0];
-    for(int i=1;i<n;i++)
-    {
-        curr = max(nums[i],curr+nums[i]);
-        maxi = max(maxi,curr);
+    // int n = nums.size();
+    // int maxi = nums[0];
+    // int curr  = nums[0];
+    // for(int i=1;i<n;i++)
+    // {
+    //     curr = max(nums[i],curr+nums[i]);
+    //     maxi = max(maxi,curr);
+    // }
+    // return maxi;
+    // }
+
+    int n=nums.size();
+    int maxi = INT_MIN;
+    int cursum=0;
+
+    for(int i:nums){
+        cursum+=i;
+        if(cursum>maxi){
+            maxi = cursum;
+        }
+        if(cursum<0){
+            cursum = 0;
+        }
+    
     }
     return maxi;
     }
