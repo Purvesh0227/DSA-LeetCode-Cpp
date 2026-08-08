2cb62fc8728a74393888ec1d20a16f9de046002f
Purvesh Mohite <purvesh0207@gmail.com>
2026-08-08 16:46:58 +0530

    Time: 0 ms (100%), Space: 28.2 MB (41.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,12 +1,34 @@
+// class Solution {
+// public:
+//     int majorityElement(vector<int>& nums) {
+//         unordered_map<int,int> count;
+//         for(int i:nums){
+//             count[i]++;
+//             if(count[i]>nums.size()/2) return i;
+//         }    
+//     return -1;
+//     }
+// };
+
+//moyre moore
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> count;
+        int count = 0;
+        int candidate = 0;
         for(int i:nums){
-            count[i]++;
-            if(count[i]>nums.size()/2) return i;
-        }    
+            if(count == 0){
+                candidate = i;
+            }
 
-    return -1;
+            if(i == candidate){
+                count+=1;
+            }
+            else
+            {
+                count-=1;
+            }
+        }
+        return candidate;
     }
 };
